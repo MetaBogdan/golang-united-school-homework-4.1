30ad8b0aa6bc8ac9342cb411eec02a21f86dc9ab
Bogdan Vozniy <Baglandir@gmail.com>
2022-05-16 11:17:14 +0800

    2 commit

--- reverse_string.go
+++ reverse_string.go
@@ -1,16 +1,16 @@
 package reverse_string
 
 import (
-	"unicode"
+	"unicode/utf8"
 )
 
 func ReverseString(input string) (output string) {
-	runeInput = []rune(input)
+	runeInput := []rune(input)
 	var finalStr string
-	lenArr := unicode.RuneCountInString(input)
-	for i := lenArr - 1; i <= 0; i-- {
+	lenArr := utf8.RuneCountInString(input)
+	for i := lenArr - 1; i >= 0; i-- {
 
-		finalStr = string(runeInput[i])
+		finalStr += string(runeInput[i])
 
 	}
 
