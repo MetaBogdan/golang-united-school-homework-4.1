077bce28c7a2071c3f1bc5289e5e1895a78958ad
Bogdan Vozniy <Baglandir@gmail.com>
2022-05-16 11:19:30 +0800

    3 commit

--- reverse_string.go
+++ reverse_string.go
@@ -9,10 +9,7 @@ func ReverseString(input string) (output string) {
 	var finalStr string
 	lenArr := utf8.RuneCountInString(input)
 	for i := lenArr - 1; i >= 0; i-- {
-
 		finalStr += string(runeInput[i])
-
 	}
-
 	return finalStr
 }
