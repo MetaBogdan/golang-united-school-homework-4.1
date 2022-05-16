ed15a58dd606446ca2e0486581532e265a6d0668
Bogdan Vozniy <Baglandir@gmail.com>
2022-05-16 11:08:09 +0800

    1 commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MetaBogdan/golang-united-school-homework-4-1
+
+go 1.18
--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,18 @@
 package reverse_string
 
+import (
+	"unicode"
+)
+
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+	runeInput = []rune(input)
+	var finalStr string
+	lenArr := unicode.RuneCountInString(input)
+	for i := lenArr - 1; i <= 0; i-- {
+
+		finalStr = string(runeInput[i])
+
+	}
+
+	return finalStr
 }
